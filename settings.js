0d1bad82ea9933188faa11103c29189790f8e140
BelluNgas <98247916+BeluNgas@users.noreply.github.com>
2022-03-18 15:39:03 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -30,7 +30,7 @@ global.ownernomerr = "+601136151330" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["601136151330","62887435047326","6285342106390"] //ganti agar fitur owner bisa di gunakan
+global.owner = ["601136151330"] //ganti agar fitur owner bisa di gunakan
 global.packname = '© Alphabot-Mdོ' //sticker wm ubah
 global.author = 'punya lungas' //sticker wm ganti nama kalian
 global.sessionName = 'session'
@@ -42,7 +42,7 @@ global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutp
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
-    sukses: '🤗Done, Oke Desu~',
+    sukses: '🤗Done, Jan lupa Donate🗿~',
     admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin !',
     botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin !',
     owner: 'Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
